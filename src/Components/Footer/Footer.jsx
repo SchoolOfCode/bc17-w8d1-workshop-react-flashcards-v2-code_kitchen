eda7d2df7ce02bbc1784b3ba0417dda0c03484e8
Ian Smythe <ianbsmythe@googlemail.com>
2024-08-19 11:11:07 +0100

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import { ReactIcon } from "../../assets/Images/ReactIcon.png"
+
+export default function Footer () {
+    return ( 
+        <footer className= "footer">
+            <p className="footerDescription"> Built with React { ReactIcon }</p>
+        </footer>
+    )
+}
