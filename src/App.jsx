6ef4c91f7e51cd2b91c43c1e4bd3f8ef75d145a4
Irene Thomas <irenethomas@Irenes-MBP.home>
2024-08-19 12:28:29 +0100

    form done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import Form from "./Components/Form/Form"
+import Form from "./Components/Form/Form";
+
 
 function App() {
   return (<>
   <Header />
+  <Form />
   <Footer />
   </>);
 }
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,10 +1,13 @@
+import "./form.css";
+
 export default function Form () {
     return (
-        <>
             <form className="form">
-                <label className="formLabelQuestion">Question:</label>
-                <label className="formLabelAnswer">Answer:</label>
+                <label for="question">Question:</label>
+                <input type="text" id="question" name="question"/> 
+                <label for="answer">Answer:</label>
+                <input type="text" id="answer" name="answer"/>
+                <input type="submit" value="Add"/>
             </form>
-        </>
     )
 }
