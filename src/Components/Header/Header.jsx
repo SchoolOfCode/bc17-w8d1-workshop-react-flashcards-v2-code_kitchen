8b9220405c7483806122be92d2d50931c2598d40
Irene Thomas <irenethomas@unknown909c4ac6f296.home>
2024-08-19 10:55:28 +0100

    header done

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return ( <header className="header" >
+<img className="headerImg" src="src/assets/ReactIcon.png" alt="React logo image" />
+<h1 className="headerTitle"> React Flashcards </h1>
+<p className="headerDescription"> 🧠 Expand Your React Knowledge, One Flashcard at a Time! 🐌</p>
+</header>
+    )
+}; 
