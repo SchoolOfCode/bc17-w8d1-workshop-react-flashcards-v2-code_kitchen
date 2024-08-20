cfdff751d307b59ca71ea61dd1d02bd10f64325b
Irene Thomas <irenethomas@Irenes-MBP.home>
2024-08-20 09:47:09 +0100

    Updated Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Form from "./Components/Form/Form";
-import Flashcards from "./Components/Flashcards/Flashcards"
-
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Form />
-      <Flashcards />
-      <Footer />
-    </>
-  );
+      const  [inputValue, setInputValue] =  useState('');
+
+    const  handleChange = (event) => {
+         //setInputValue(event.target.value);
+         console.log(event);
+         };
+
+  return (<>
+  <Header />
+  <Form value={inputValue} onChange={handleChange}/> 
+  <Footer />
+  </>);
 }
 
 export default App;
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,12 +1,17 @@
 import "./form.css";
 
-export default function Form () {
+
+export default function Form (props) {
+
     return (
             <form className="form">
+
                 <label for="question">Question:</label>
-                <input type="text" id="question" name="question"/> 
+                <input type="text" id="question" name="question" onChange={props.onChange}/> 
+                
                 <label for="answer">Answer:</label>
-                <input type="text" id="answer" name="answer"/>
+                <input type="text" id="answer" name="answer" onChange={props.onChange}/>
+                
                 <input type="submit" value="Add"/>
             </form>
     )
